ea5ec0c481928f57917c93d4ba812b2a8a6dca47
laura julien <laura.julien@ymail.com>
2021-02-20 15:09:08 +0100

    stage model

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,3 +1,6 @@
 class Cocktail < ApplicationRecord
-  has_many :doses
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,5 @@
 class Ingredient < ApplicationRecord
   has_many :doses
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
